b7b05b8304349704016ea8e16a4bd9569c17ff54
Sukhmeet Singh <88.sukhmeet.singh@gmail.com>
2024-03-27 22:39:10 +0000

    inital push

---
+++ main.py
@@ -0,0 +1,14 @@
+print("hello world")
+x=15
+print (x)
+price =10
+print(price)
+result = price * (1-0.2)
+print (result)
+name="sukh"
+print ( name + name)
+
+print (f"hello {name}")
+greeting = "hello, {}"
+with_name = greeting.format(name)
+print (with_name)
