41e1299be811b6b0ffb2dbe8d841f02b19bf457a
Sukhmeet Singh <88.sukhmeet.singh@gmail.com>
2024-03-27 23:31:19 +0000

    lists_tuples

---
+++ lists_tuples/code.py
@@ -0,0 +1,12 @@
+list= ["sukhmeet", "singh", "python"]
+print (list[0])
+tuple = ("Sukhmeet", "Singh")
+print (tuple[1])
+set= {1,2,3} 
+print (set)
+list.append("git push")
+print (list)
+set2 = {2,3,4}
+print(set.difference(set2))
+print(set.intersection(set2))
+single_tuple= (1,)
---
+++ varinput.py
@@ -0,0 +1,8 @@
+size_input= input("how big is your house:(in square meter):")
+square_feet = int(size_input)
+square_meters=square_feet/10.8
+print(f"{square_feet} square feet is {square_meters} square_meters.")
+name = 'Bob'
+my_string = f'Hello {name}!'
+name = 'Rolf'
+print(my_string)
